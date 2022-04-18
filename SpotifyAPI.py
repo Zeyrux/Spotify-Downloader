e680055c102e4d0344ee5097e5b7a49b735c3555
Zeyrux <tappetheo@gmail.com>
2022-04-18 23:42:07 +0200

    added get_playlist to SpotifyAPI

---
+++ SpotifyAPI.py
@@ -0,0 +1,66 @@
+import datetime as dt
+import base64
+
+from urllib.parse import urlencode
+from urllib.parse import urlparse
+import requests
+
+
+class AccessToken:
+    def __init__(self, access_token: str, token_type: str, expires_in: float):
+        self.token = access_token
+        self.token_type = token_type
+        self.expires = dt.datetime.now() + dt.timedelta(seconds=expires_in)
+
+    @staticmethod
+    def from_json(data):
+        return AccessToken(data["access_token"],
+                           data["token_type"],
+                           data["expires_in"])
+
+    def is_expired(self):
+        return dt.datetime.now() > self.expires
+
+
+class SpotifyAPI:
+
+    URL_TOKEN = "https://accounts.spotify.com/api/token"
+    URL_SEARCH = "https://api.spotify.com/v1/search"
+
+    TOKEN_DATA = {"grant_type": "client_credentials"}
+
+    def __init__(self, client_id: str, client_secret: str):
+        self.CLIENT_ID = client_id
+        self.CLIENT_SECRET = client_secret
+        self.CLIENT_CREDS = f"{client_id}:{client_secret}"
+        self.CLIENT_CREDS_64 = (base64.b64encode(
+            self.CLIENT_CREDS.encode()
+        )).decode()
+        self.HEADERS_AUTHORIZE = {"Authorization":
+                                      f"basic {self.CLIENT_CREDS_64}"}
+
+        self.access_token = self._authorize()
+
+        self.HEADERS_SEARCH = {"Authorization":
+                                   f"{self.access_token.token_type} "
+                                   f"{self.access_token.token}"}
+
+    def _authorize(self) -> AccessToken:
+        response = requests.post(self.URL_TOKEN,
+                                 data=self.TOKEN_DATA,
+                                 headers=self.HEADERS_AUTHORIZE)
+        if response.status_code not in range(200, 299):
+            raise Exception(f"could not authorizse STATUS: {response.status_code}")
+        return AccessToken.from_json(response.json())
+
+    def search(self, query: str, search_type: str) -> dict:
+        data = urlencode({"q": query, "type": search_type})
+        url = f"{self.URL_SEARCH}?{data}"
+        return requests.get(url, headers=self.HEADERS_SEARCH).json()
+
+    def get_playlist(self, url: str) -> dict:
+        playlist_id = urlparse(url).path.split("/")[-1]
+        return requests.get(
+            f"https://api.spotify.com/v1/playlists/{playlist_id}",
+            headers=self.HEADERS_SEARCH
+        ).json()
--- main.py
+++ main.py
@@ -1,64 +1,6 @@
-import datetime as dt
-import base64
 import os
 
-from urllib.parse import urlencode
-import spotipy
-import requests
-
-
-class AccessToken:
-    def __init__(self, access_token: str, token_type: str, expires_in: float):
-        self.token = access_token
-        self.token_type = token_type
-        self.expires = dt.datetime.now() + dt.timedelta(seconds=expires_in)
-
-    @staticmethod
-    def from_json(data):
-        return AccessToken(data["access_token"],
-                           data["token_type"],
-                           data["expires_in"])
-
-    def is_expired(self):
-        return dt.datetime.now() > self.expires
-
-
-class SpotifyAPI:
-
-    URL_TOKEN = "https://accounts.spotify.com/api/token"
-    URL_SEARCH = "https://api.spotify.com/v1/search"
-
-    TOKEN_DATA = {"grant_type": "client_credentials"}
-
-    def __init__(self, client_id: str, client_secret: str):
-        self.CLIENT_ID = client_id
-        self.CLIENT_SECRET = client_secret
-        self.CLIENT_CREDS = f"{client_id}:{client_secret}"
-        self.CLIENT_CREDS_64 = (base64.b64encode(
-            self.CLIENT_CREDS.encode()
-        )).decode()
-        self.HEADERS_AUTHORIZE = {"Authorization":
-                                      f"Basic {self.CLIENT_CREDS_64}"}
-
-        self.access_token = self._authorize()
-
-        self.HEADERS_SEARCH = {"Authorization":
-                                   f"{self.access_token.token_type} "
-                                   f"{self.access_token.token}"}
-
-    def _authorize(self) -> AccessToken:
-        response = requests.post(self.URL_TOKEN,
-                                 data=self.TOKEN_DATA,
-                                 headers=self.HEADERS_AUTHORIZE)
-        if response.status_code not in range(200, 299):
-            raise Exception(f"could not authorizse STATUS: {response.status_code}")
-        return AccessToken.from_json(response.json())
-
-    def search(self, query: str, search_type: str):
-        data = urlencode({"q": query, "type": search_type})
-        url = f"{self.URL_SEARCH}?{data}"
-        r = requests.get(url, headers=self.HEADERS_SEARCH)
-        return r
+from SpotifyAPI import SpotifyAPI
 
 
 def get_client_id_and_secret(path="client.txt") -> tuple:
@@ -69,7 +11,16 @@ def get_client_id_and_secret(path="client.txt") -> tuple:
 
 def main():
     client_id, client_secret = get_client_id_and_secret()
-    print(SpotifyAPI(client_id, client_secret).search("Remedy", "track").json())
+    a = SpotifyAPI(client_id, client_secret).get_playlist("https://open.spotify.com/playlist/0M5W3EwBG1ewC2ky7ND034?si=f112f80f11004560")
+    b = a["tracks"]["items"]
+    for c in b:
+        print(f'{c["track"]["name"]}')
+        print(f'{c["track"]["external_urls"]["spotify"]}')
+        artists = c["track"]["album"]["artists"]
+        print("Artists:")
+        for artist in artists:
+            print(f'\t{artist["name"]}')
+        print()
 
 
 if __name__ == '__main__':
