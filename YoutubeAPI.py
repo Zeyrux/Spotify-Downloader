5597eba904202f5e517618860bce358a1e1c296a
Zeyrux <tappetheo@gmail.com>
2022-04-19 14:03:47 +0200

    added YoutubeAPI

---
+++ YoutubeAPI.py
@@ -0,0 +1,37 @@
+from googleapiclient.discovery import build
+import pytube
+
+
+class YoutubeAPI:
+    def __init__(self, api_key):
+        self.API_KEY = api_key
+        self.youtube = build("youtube", "v3", developerKey=self.API_KEY)
+
+    def search_video(self,
+                     q,
+                     type="video",
+                     maxResults=50,
+                     videoDuration=None) -> dict:
+        args = {"q": q, "type": type, "maxResults": maxResults}
+        if videoDuration is not None:
+            args["video_duration"] = videoDuration
+        return self.youtube.Search().list(**args)
+
+    def search_song(self,
+                    track_name: str,
+                    artists: list = None,
+                    video_duration: int = None,
+                    max_results = 50
+                    ) -> tuple[pytube.YouTube, dict]:
+        q = f"(intitle:{track_name})"
+        if artists is not None:
+            for artist in artists:
+                q += f"(intitle:{artist})"
+        args = {"q": q, "maxResults": max_results}
+        tracks = self.youtube.search().list(part="snippet", **args).execute()
+        for track in tracks["items"]:
+            track_id = track["id"]["videoId"]
+            url = f"https://www.youtube.com/watch?v={track_id}"
+            track_object = pytube.YouTube(url)
+            if video_duration - 5 < track_object.length < video_duration + 5:
+                return track_object, track
--- main.py
+++ main.py
@@ -1,26 +1,44 @@
 import os
 
 from SpotifyAPI import SpotifyAPI
+from YoutubeAPI import YoutubeAPI
 
 
-def get_client_id_and_secret(path="client.txt") -> tuple:
+def get_spotify_client_id_and_secret(path="spotify_api_keys.txt") -> tuple:
     if not os.path.isfile(path):
         raise FileNotFoundError(path)
     return tuple(open(path, "r").read().split("\n"))
 
 
+def get_youtube_api_key(path="youtube_api_keys.txt"):
+    return open(path, "r").read()
+
+
 def main():
-    client_id, client_secret = get_client_id_and_secret()
-    a = SpotifyAPI(client_id, client_secret).get_playlist("https://open.spotify.com/playlist/0M5W3EwBG1ewC2ky7ND034?si=f112f80f11004560")
-    b = a["tracks"]["items"]
-    for c in b:
-        print(f'{c["track"]["name"]}')
-        print(f'{c["track"]["external_urls"]["spotify"]}')
-        artists = c["track"]["album"]["artists"]
-        print("Artists:")
+    playlist_url = input("Playlist URL: ")
+    playlist_url = "https://open.spotify.com/playlist/0M5W3EwBG1ewC2ky7ND034?si=f112f80f11004560"
+
+    spotify_api_id, spotify_api_secret = get_spotify_client_id_and_secret()
+    spotify_response = SpotifyAPI(
+        spotify_api_id,
+        spotify_api_secret
+    ).get_playlist(playlist_url)
+
+    youtube_api_key = get_youtube_api_key()
+    spotify_tracks = spotify_response["tracks"]["items"]
+    for spotify_track in spotify_tracks:
+        duration = spotify_track["track"]["duration_ms"] // 1000
+        track_name = spotify_track["track"]["name"]
+        artists = spotify_track["track"]["album"]["artists"]
+        artists_name = []
         for artist in artists:
-            print(f'\t{artist["name"]}')
-        print()
+            artists_name.append(artist["name"])
+
+        pytube_track, _ = YoutubeAPI(youtube_api_key).search_song(track_name,
+                                                                  artists_name,
+                                                                  duration)
+        # print(pt_object.thumbnail_url)
+        # break
 
 
 if __name__ == '__main__':
